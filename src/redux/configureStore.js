165108cecacbc7b1e7bf39ce5e7b52df851480f2
bxsck <bosstk05nya@gmail.com>
2020-10-21 12:40:57 +0700

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { DISHES } from './dishes';
+import { COMMENTS } from './comments';
+import { PROMOTIONS } from './promotions';
+import { LEADERS } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        combineReducers({
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        })
     );
 
     return store;
